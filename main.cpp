347a1c2bad21c437fe9e1a11c45a95915a5b11f7
Rhea Sanjay <117992159+rhxyikes@users.noreply.github.com>
2024-03-05 00:24:42 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <unordered_map>
 #include <fstream> 
 #include <string> 
+#include <ctype.h>
+#include <bitset>
 using namespace std;
 int main()
 {
@@ -29,6 +31,98 @@ int main()
 
     U_format["auipc"]={"0010111"};
     U_format["lui"]={"0110111"};
+
+    //creating an unordered_map to store I_format basic instruction codes (opcode and funct3)
+    unordered_map<string,vector<string>> I_format;
+    //adding addi instruction 
+    vector<string> addi;
+    addi.push_back("0010011");//opcode
+    addi.push_back("000");//funct3
+    I_format["addi"] = addi;
+
+    //adding andi instruction 
+    vector<string> andi;
+    andi.push_back("0010011");//opcode
+    andi.push_back("111");//funct3
+    I_format["andi"] = andi;
+
+    //adding ori instruction 
+    vector<string> ori;
+    ori.push_back("0010011");//opcode
+    ori.push_back("110");//funct3
+    I_format["ori"] = ori;
+
+    //adding lb instruction 
+    vector<string> lb;
+    lb.push_back("0000011");//opcode
+    lb.push_back("000");//funct3
+    I_format["lb"] = lb;
+
+    //adding lh instruction 
+    vector<string> lh;
+    lh.push_back("0000011");//opcode
+    lh.push_back("001");//funct3
+    I_format["lh"] = lh;
+
+    //adding lw instruction 
+    vector<string> lw;
+    lw.push_back("0000011");//opcode
+    lw.push_back("010");//funct3
+    I_format["lw"] = lw;
+
+    //adding ld instruction 
+    vector<string> ld;
+    ld.push_back("0000011");//opcode
+    ld.push_back("011");//funct3
+    I_format["ld"] = ld;
+
+    //adding jalr instruction 
+    vector<string> jalr;
+    jalr.push_back("1100111");//opcode
+    jalr.push_back("000");//funct3
+    I_format["jalr"] = jalr;
+
+    /*
+        for(auto x: I_format)
+    {
+        cout<<x.first<<endl;
+        for(auto y: x.second)
+        {
+            cout<<y<<endl;
+        }
+    }
+    */
+
+    //creating an unordered_map to store SB_format basic instruction codes (opcode and funct3)
+    unordered_map<string,vector<int>>SB_format;
+
+    //adding sb instruction 
+    vector<string> sb;
+    sb.push_back("0100011");//opcode
+    sb.push_back("000");//funct3
+    I_format["sb"] = sb;
+
+    //adding sh instruction 
+    vector<string> sh;
+    sh.push_back("0100011");//opcode
+    sh.push_back("001");//funct3
+    I_format["sh"] = sh;
+
+    //adding sw instruction 
+    vector<string> sw;
+    sw.push_back("0100011");//opcode
+    sw.push_back("010");//funct3
+    I_format["sw"] = sw;
+
+    //adding sd instruction 
+    vector<string> sd;
+    sd.push_back("0100011");//opcode
+    sd.push_back("011");//funct3
+    I_format["sd"] = sd;
+
+    //creating an unordered_map to store UJ_format basic instruction codes (opcode)
+    unordered_map<string,string>UJ_format;
+    UJ_format["jal"] = "1101111";
 
     ifstream file("input_file.asm"); 
     string line; 
@@ -60,6 +154,93 @@ int main()
             }
         }  
         file.close(); 
-    } 
+    }
+    unordered_map<string,vector<string>> I_format;
+    vector<string> input;
+    int size = input.size();
+    string data = ".data";
+    string text = ".text";
+    auto it_data = find(input.begin(),input.end(),data);
+    auto it_text = find(input.begin(),input.end(),text);
+    int start = 0;
+    //finding the text segment
+    if(it_data != input.end())
+    {
+        int i = 0;
+        while(input[i]!=text) i++;
+        start = i+1;
+    }
+    else if (input[start]==text) start = start+1;
+    // program counter
+    int PC = 0;
+    //reading the instructions line by line
+    for(int i=start;i<size;i++)
+    {
+        string line = input[i];
+        int j =0;
+        if(line[j] == ' ') j++;
+        string instruction = "";
+        while(line[j]!= ' ')
+        {
+            instruction += line[j];
+            j++;
+        }
+        if(I_format.find(instruction)!=I_format.end())
+        {
+            //getting opcode
+            string opcode = I_format[instruction][0];
 
+            //getting funct3
+            string funct3 = I_format[instruction][1];
+
+            //getting rs1
+            j++;
+            int reg = 0;
+            while(line[j]!=',')
+            {
+                if(isdigit(line[j]))
+                {
+                    reg = reg*10 + (int(line[j]-'0'));
+                }
+                j++;
+            }
+            bitset<5> rs1_bits(reg);
+            string rs1 = rs1_bits.to_string();
+
+            //getting rd
+            j++;
+            reg = 0;
+            while(line[j]!=',')
+            {
+                if(isdigit(line[j]))
+                {
+                    reg = reg*10 + (int(line[j]-'0'));
+                }
+                j++;
+            }
+            bitset<5> rd_bits(reg);
+            string rd = rd_bits.to_string();
+
+            //getting immediate value
+            j++;
+            int immed = 0;
+            int neg = 0;
+            while(line[j]!='\n')
+            {
+                if(isdigit(line[j]))
+                {
+                    immed = immed*10 + (int(line[j]-'0'));
+                }
+                if(line[j]=='-')
+                {
+                    neg = 1;
+                }
+                j++;
+            }
+            if(neg==1) immed *= -1;
+            
+        }
+
+    }
+    return 0;
 }
